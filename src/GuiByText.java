b3f399037838fa800cb4f261d0b618c64514bc8a
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-05-08 01:09:44 -0700

    GuiByText FIX

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -8,6 +8,15 @@ public class GuiByText extends JFrame {
     JTextField textField1 = new JTextField();
     JButton button1 = new JButton("Greet");
 
+    JLabel label2 = new JLabel("Panjang");
+    JLabel label3 = new JLabel("Lebar");
+    JLabel label4 = new JLabel("Hasil");
+    JTextField tPanjang = new JTextField();
+    JTextField tLebar = new JTextField();
+    JTextField tHasil = new JTextField();
+    JButton bHitung = new JButton("Hitung Luas");
+
+
     GuiByText(){
         setSize(640, 480);
         setLocationRelativeTo(null);
@@ -22,10 +31,25 @@ public class GuiByText extends JFrame {
         getContentPane().add(label);
         getContentPane().add(textField1);
         getContentPane().add(button1);
+        getContentPane().add(label2);
+        getContentPane().add(label3);
+        getContentPane().add(label4);
+        getContentPane().add(tPanjang);
+        getContentPane().add(tLebar);
+        getContentPane().add(tHasil);
+        getContentPane().add(bHitung);
 
         label.setBounds(16,32,100,25);
         textField1.setBounds(124,32,100,25);
         button1.setBounds(232,32,80,25);
+
+        label2.setBounds(16,80,100,25);
+        tPanjang.setBounds(124,80,100,25);
+        label3.setBounds(16,113,100,25);
+        tLebar.setBounds(124,113,100,25);
+        bHitung.setBounds(16,146,208,25);
+        label4.setBounds(16,179,100,25);
+        tHasil.setBounds(124,179,100,25);
     }
     void menambahkanListener(){
         button1.addActionListener(new ActionListener() {
@@ -36,6 +60,19 @@ public class GuiByText extends JFrame {
             }
         });
     }
+    void menghitung(){
+        bHitung.addActionListener(actionEvent -> {
+            int panjang = Integer.parseInt(tPanjang.getText());
+            int lebar = Integer.parseInt(tLebar.getText());
+
+            int hasil = panjang * lebar;
+            String sHasil =  String.valueOf(hasil);
+
+            tHasil.setText(sHasil);
+        });
+    }
+
+
     public static void main(String[] args) {
         GuiByText guiByText = new GuiByText();
         guiByText.meletakkanKomponen();
