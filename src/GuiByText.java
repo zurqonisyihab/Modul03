c68f9f2ed62a4ed9dc7c66ce7425befb608ee8c4
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-05-07 23:25:42 -0700

    GuiByText mantap

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -1,6 +1,13 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuiByText extends JFrame {
+
+    JLabel label = new JLabel( "Cobs Input 1");
+    JTextField textField1 = new JTextField();
+    JButton button1 = new JButton("Greet");
+
     GuiByText(){
         setSize(640, 480);
         setLocationRelativeTo(null);
@@ -9,8 +16,29 @@ public class GuiByText extends JFrame {
 
     }
 
+    void meletakkanKomponen(){
+        getContentPane().setLayout(null);
+
+        getContentPane().add(label);
+        getContentPane().add(textField1);
+        getContentPane().add(button1);
+
+        label.setBounds(16,32,100,25);
+        textField1.setBounds(124,32,100,25);
+        button1.setBounds(232,32,80,25);
+    }
+    void menambahkanListener(){
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nama = textField1.getText();
+                JOptionPane.showMessageDialog(null, "Hai " + nama);
+            }
+        });
+    }
     public static void main(String[] args) {
         GuiByText guiByText = new GuiByText();
+        guiByText.meletakkanKomponen();
         guiByText.setVisible(true);
 
     }
