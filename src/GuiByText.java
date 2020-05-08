e5fde158f7a7678d11a30f6223caa1dd41068d96
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-05-07 22:38:35 -0700

    Belajar GuiByText

---
+++ src/GuiByText.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GuiByText extends JFrame {
+    GuiByText(){
+        setSize(640, 480);
+
+    }
+
+    public static void main(String[] args) {
+        GuiByText guiByText = new GuiByText();
+        guiByText.setVisible(true);
+
+    }
+}
